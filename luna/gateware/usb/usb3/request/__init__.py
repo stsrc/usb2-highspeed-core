7b70ad026d37bd5577be45396dac82a7844b6649
Hans Baier <hansfbaier@gmail.com>
2021-07-29 07:15:09 +0700

    remove USB3 parts

--- luna/gateware/usb/usb3/request/__init__.py
+++
@@ -1,6 +0,0 @@
-#
-# This file is part of LUNA.
-#
-# Copyright (c) 2020 Great Scott Gadgets <info@greatscottgadgets.com>
-# SPDX-License-Identifier: BSD-3-Clause
-""" USB3 control request handler gateware. """
